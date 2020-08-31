8c335b0b54c1c3a26f745af1a07d219818f74fbf
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-08-31 00:09:36 -0500

    useState & useEffect in ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles"
 
-import { categories } from "../../../api/db.json"
 
-export const ListOfCategories = ({ cover, emoji }) => {
+
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function () {
+    fetch('https://dgotv2-server.vercel.app/categories')
+      .then(response => response.json())
+      .then(data => {
+        setCategories(data)
+      })
+  }, [])
+
   return (
     <List>
       {
